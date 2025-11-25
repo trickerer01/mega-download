55f8e65058e05257a953fd0a943d4c3a32ecc68f
trickerer01 <onlysuffering+1@gmail.com>
2025-11-25 13:37:47 +0700

    Add download tests

--- mega_download/util/strings.py
+++ mega_download/util/strings.py
@@ -8,10 +8,34 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 
 import urllib.parse
 
+HTTP_PREFIX = 'http://'
+HTTPS_PREFIX = 'https://'
+SITE_PRIMARY = f'{HTTPS_PREFIX}mega.nz'
+SITE_API = f'{HTTPS_PREFIX}g.api.mega.co.nz/cs'
+
 
 def ensure_scheme_https(url: str) -> str:
     if urllib.parse.urlparse(url).scheme == 'http':
-        return url.replace('http://', 'https://')
+        return url.replace(HTTP_PREFIX, HTTPS_PREFIX)
+
+
+def compose_link_v2(folder_id: str, file_id: str, key: str) -> str:
+    """
+    :param folder_id: [str] mega folder id (if any)
+    :param file_id: [str] mega file id (if any)
+    :param key: [str] mega decryption key
+    :returns: composed file OR folder OR folder file link (version 2)
+    """
+    if key and (folder_id or file_id):
+        if folder_id:
+            link = f'{SITE_PRIMARY}/folder/{folder_id}#{key}'
+            if file_id:
+                link = f'{link}/file/{file_id}'
+        else:  # if file_id
+            link = f'{SITE_PRIMARY}/file/{file_id}#{key}'
+        return link
+    # invalid link always
+    return ''
 
 #
 #
--- tests/tests.py
+++ tests/tests.py
@@ -7,11 +7,17 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 
 import functools
+import pathlib
 from collections.abc import Callable
+from tempfile import TemporaryDirectory
 from unittest import TestCase
 
+from mega_download import APP_NAME, main_sync
+from mega_download.api import DownloadMode
 from mega_download.config import Config
+from mega_download.defs import LoggingFlags
 from mega_download.logger import Log
+from mega_download.util.strings import compose_link_v2
 
 RUN_CONN_TESTS = 0
 
@@ -33,6 +39,86 @@ class CmdTests(TestCase):
     @test_prepare()
     def test_config_integrity(self):
         assert all(hasattr(Config, _) for _ in Config.NAMESPACE_VARS_REMAP.values())
+        print(f'{self._testMethodName} passed')
+
+
+class DownloadTouchTests(TestCase):
+    @test_prepare()
+    def test_download_touch_1(self):
+        if not RUN_CONN_TESTS:
+            return
+        with TemporaryDirectory(prefix=f'{APP_NAME}_{self._testMethodName}_') as tempdir_name:
+            tempfile_name = 'oem_mx/c_p_mx.edf'
+            tempfile_path = pathlib.Path(tempdir_name) / tempfile_name
+            templink = compose_link_v2('6cE1AazL', 'jAsE1BiZ', '9ISiN871PS8mBAKIkiSLdw')
+            arglist = [
+                templink,
+                '-o', tempfile_path.parent.parent.as_posix(),
+                '-d', DownloadMode.TOUCH.value,
+                '-v', LoggingFlags.INFO.name.lower(),
+            ]
+            main_sync(arglist)
+            self.assertTrue(tempfile_path.exists())
+            self.assertEqual(0, tempfile_path.stat().st_size)
+        print(f'{self._testMethodName} passed')
+
+    @test_prepare()
+    def test_download_touch_2(self):
+        if not RUN_CONN_TESTS:
+            return
+        with TemporaryDirectory(prefix=f'{APP_NAME}_{self._testMethodName}_') as tempdir_name:
+            tempfile_name = 'KTM_CBF_18.pnt'
+            tempfile_path = pathlib.Path(tempdir_name) / tempfile_name
+            templink = compose_link_v2('', 'WVczzLpB', 'faIpcuNadIMU4uLyNQ1LfWFhAHBwPewtxxSiTIdeHWI')
+            arglist = [
+                templink,
+                '-o', tempfile_path.parent.as_posix(),
+                '-d', DownloadMode.TOUCH.value,
+                '-v', LoggingFlags.INFO.name.lower(),
+            ]
+            main_sync(arglist)
+            self.assertTrue(tempfile_path.exists())
+            self.assertEqual(0, tempfile_path.stat().st_size)
+        print(f'{self._testMethodName} passed')
+
+
+class DownloadFullTests(TestCase):
+    @test_prepare()
+    def test_download_full_1(self):
+        if not RUN_CONN_TESTS:
+            return
+        with TemporaryDirectory(prefix=f'{APP_NAME}_{self._testMethodName}_') as tempdir_name:
+            tempfile_name = 'oem_mx/p_mx_s.edf'
+            tempfile_path = pathlib.Path(tempdir_name) / tempfile_name
+            templink = compose_link_v2('6cE1AazL', 'qZsWyJ5K', '9ISiN871PS8mBAKIkiSLdw')
+            arglist = [
+                templink,
+                '-o', tempfile_path.parent.parent.as_posix(),
+                '-d', DownloadMode.FULL.value,
+                '-v', LoggingFlags.INFO.name.lower(),
+            ]
+            main_sync(arglist)
+            self.assertTrue(tempfile_path.exists())
+            self.assertEqual(1172614, tempfile_path.stat().st_size)
+        print(f'{self._testMethodName} passed')
+
+    @test_prepare()
+    def test_download_full_2(self):
+        if not RUN_CONN_TESTS:
+            return
+        with TemporaryDirectory(prefix=f'{APP_NAME}_{self._testMethodName}_') as tempdir_name:
+            tempfile_name = 'KTM_CBF_PUB.pnt'
+            tempfile_path = pathlib.Path(tempdir_name) / tempfile_name
+            templink = compose_link_v2('', 'PZFCVYYK', 'El6KmJzu_Z763NzYrLlCUdD_v2W1thlw22B-w3ko7u8')
+            arglist = [
+                templink,
+                '-o', tempfile_path.parent.as_posix(),
+                '-d', DownloadMode.FULL.value,
+                '-v', LoggingFlags.INFO.name.lower(),
+            ]
+            main_sync(arglist)
+            self.assertTrue(tempfile_path.exists())
+            self.assertEqual(5040037, tempfile_path.stat().st_size)
         print(f'{self._testMethodName} passed')
 
 #
